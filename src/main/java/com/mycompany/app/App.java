ecfa347f653c74276d48d84c38ae55c7315474c8
Mohammad Piyarji <136907810+Maddy9786@users.noreply.github.com>
2024-07-18 11:42:27 +0530

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello fani World!" );
+        System.out.println( "Hello fani World this world is useless!" );
     }
 }
